affd170c54dff282dd37b8130edc30fa9beb2af0
HotelPartners <hotelpartnersd3sunbeam@gmail.com>
2023-09-17 15:45:34 +0530

    UI Changed

--- React/my-app/src/Pages/Quotes.js
+++ React/my-app/src/Pages/Quotes.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import { MDBCard, MDBCardHeader, MDBCardBody, MDBTypography } from 'mdb-react-ui-kit';
 import Header from "../Header";
 
 function Quotes() {
@@ -23,7 +24,7 @@ function Quotes() {
 
     return (<>
         <Header />
-        <div className="table-responsive">
+        {/* <div className="table-responsive">
             <table className="table table-bordered">
                 <thead>
                     <tr>
@@ -44,7 +45,25 @@ function Quotes() {
                     }
                 </tbody>
             </table>
-        </div>
+        </div> */}
+
+        {quotes.map((quote) => (<>
+            <center>
+                <MDBCard className="m-3 w-50" key={quote.quote_id}>
+                    <MDBCardHeader>{quote.quote_id}</MDBCardHeader>
+                    <MDBCardBody>
+                        <MDBTypography blockquote className='mb-0'>
+                            <p>{quote.text}</p>
+                            <footer className='blockquote-footer'>
+                                {quote.author}
+                            </footer>
+                        </MDBTypography>
+                    </MDBCardBody>
+                </MDBCard>
+            </center>
+        </>))}
+
+
 
 
     </>);
